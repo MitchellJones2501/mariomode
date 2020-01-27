8e398fd0a21bb6b1936d9ea3a0c299c0f580a78c
Mitchell Jones <57398776+MitchellJones2501@users.noreply.github.com>
2020-01-27 17:04:13 +0000

    Week 3

---
+++ MarioProject/Commons.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Vector2D
+{
+	float X;
+	float Y;
+
+	Vector2D(float x, float y)
+
+	{
+		X = x;
+		Y = y;
+	}
+};
---
+++ MarioProject/Texture.h
@@ -0,0 +1,4 @@
+#pragma once
+#ifndef _TEXTURE2D_H
+#define _TEXTURE2D_H
+#endif _TEXTIRE2D_H
